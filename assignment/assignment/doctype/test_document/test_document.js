2f9d9d00fddcfa9d5ed64da351e821841e12400b
dharanidharansr <samidharani07@gmail.com>
2026-07-31 12:23:41 +0530

    Controller Lifecycle (Python) have implemented successfully

---
+++ assignment/assignment/doctype/test_document/test_document.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, CR7 and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test Document", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ assignment/assignment/doctype/test_document/test_document.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2026, CR7 and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TestDocument(Document):
+
+	def before_save(self):
+		if not self.description:
+			self.description="Default Description"
