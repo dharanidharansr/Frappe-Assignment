5422833e07f028f581991fe6ac86ef20f3bbe722
dharanidharansr <samidharani07@gmail.com>
2026-08-25 09:30:31 +0000

    done the custom apps hello-app

--- assignment/commands.py
+++ assignment/commands.py
@@ -1,7 +1,14 @@
 import click
 
+
 @click.command()
 def wow():
     click.echo("Hello from the custom Bench CLI!!")
-    
-commands = [wow]
+
+
+@click.command("hello-app")
+def hello_app():
+    click.echo("Hello from custom command!")
+
+
+commands = [wow, hello_app]
